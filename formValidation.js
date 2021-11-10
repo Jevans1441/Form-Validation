941057ada63d651aa6dbc0baf84fe70f9a7530b5
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-11-09 20:44:07 -0500

    Initial commit

---
+++ formValidation.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+const formUsername = document.querySelector("#formUsername");
+
+
+const validateInputs = () => {
+    if 
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const ageNumber = document.querySelector("#ageNumber");
+    
+    if (!)
+});
