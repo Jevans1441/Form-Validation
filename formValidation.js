2264b92290d425074f38bbb897a9650df449ca72
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-11-09 22:07:02 -0500

    added CSS

--- formValidation.js
+++ formValidation.js
@@ -1,14 +1,1 @@
-const form = document.querySelector("form");
-const formUsername = document.querySelector("#formUsername");
 
-
-c
-
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    const ageNumber = document.querySelector("#ageNumber");
-    
-    if (!)
-});
